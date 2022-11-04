49b1a359a22929713758d26a96b3dd477226074c
valteriomon <wally.pg@outlook.com>
2022-11-03 21:32:15 -0300

    added command for text conversion

--- main.ts
+++ main.ts
@@ -10,34 +10,49 @@ export default class MarkdownToHTML extends Plugin {
 		this.addCommand({
 			id: 'copy-as-html-command',
 			name: 'Copy as HTML command',
-			editorCallback: (editor: any) => this.markdownToHTML(editor)
+			editorCallback: (editor: any) => this.convertMarkdown(editor, false)
 		});
 
-		}
+		this.addCommand({
+			id: 'copy-as-text-command',
+			name: 'Copy as text command',
+			editorCallback: (editor: any) => this.convertMarkdown(editor, true)
+		});
 
-		markdownToHTML(editor: Editor) {
-			const converter = new showdown.Converter();
-			let text = editor.getSelection();
-			let noBrackets = text.replace(/\[\[(?:[^\]]+\|)?([^\]]+)\]\]/g, '$1');
-			let html = converter.makeHtml(noBrackets).toString();
-			const withDivWrapper = `<!-- directives:[] -->
-			<div id="content">${html}</div>`;
-			//@ts-ignore
-			const blob = new Blob([withDivWrapper], {
-				//@ts-ignore
-				type: ["text/plain", "text/html"]
-			  })
-			const data = [new ClipboardItem({
-				//@ts-ignore
-				["text/plain"]: blob,
-				//@ts-ignore
-				["text/html"]: blob
-			  })];
-			//@ts-ignore
-			navigator.clipboard.write(data);
-		}
-
-		onunload() {
-	
-		}
 	}
+
+	convertMarkdown(editor: Editor, convertToText: boolean) {
+		const converter = new showdown.Converter();
+		let text = editor.getSelection() ? editor.getSelection() : editor.getLine(editor.getCursor().line);
+		let noBrackets = text.replace(/\[\[(?:[^\]]+\|)?([^\]]+)\]\]/g, '$1');
+		const html = converter.makeHtml(noBrackets).toString();
+
+		let convertedContent;
+		if(convertToText) {
+			const div = document.createElement("div");
+    		div.innerHTML = html;
+			convertedContent = div.textContent;
+		} else {
+			convertedContent = `<!-- directives:[] -->
+			<div id="content">${html}</div>`;
+		}
+
+		//@ts-ignore
+		const blob = new Blob([convertedContent], {
+			//@ts-ignore
+			type: ["text/plain", "text/html"]
+			})
+		const data = [new ClipboardItem({
+			//@ts-ignore
+			["text/plain"]: blob,
+			//@ts-ignore
+			["text/html"]: blob
+			})];
+		//@ts-ignore
+		navigator.clipboard.write(data);
+	}
+
+	onunload() {
+
+	}
+}
