c85f973a1f952981a0795e2b7a90c192a49b719b
Bailey Jennings <jenningsebailey@gmail.com>
2023-05-21 09:36:40 -0400

    adding support for settings

--- main.ts
+++ main.ts
@@ -1,29 +1,52 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 import * as showdown from 'showdown';
 
-// Remember to rename these classes and interfaces!
+interface MarkdownToHTMLSettings {
+    removeBrackets: boolean;
+    removeEmphasis: boolean;
+    removeTags: boolean;
+  }
+  
+
+  const DEFAULT_SETTINGS: MarkdownToHTMLSettings = {
+    removeBrackets: true,
+    removeEmphasis: false,
+    removeTags: false,
+  };
 
 export default class MarkdownToHTML extends Plugin {
-	async onload() {
+    settings: MarkdownToHTMLSettings;
 
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'copy-as-html-command',
-			name: 'Copy as HTML command',
-			editorCallback: (editor: any) => this.markdownToHTML(editor)
-		});
+    async onload() {
+        await this.loadSettings();
+        this.addCommand({
+            id: 'copy-as-html-command',
+            name: 'Copy as HTML command',
+            editorCallback: (editor: any) => this.markdownToHTML(editor)
+        });
+        this.addSettingTab(new MarkdownToHTMLSettingTab(this.app, this));
+    }
 
-	}
+    markdownToHTML(editor: Editor) {
+        const converter = new showdown.Converter();
+        converter.setFlavor('github');
+        converter.setOption('ellipsis', false);
+        let text = editor.getSelection();
+        if (this.settings.removeBrackets) {
+            text = text.replace(/\[\[(?:.*\/)?(?:[^\]]+\|)?([^\]]+)\]\]/g, '$1');
+          }
+          
+        if (this.settings.removeEmphasis) {
+            text = text.replace(/[*=]+(\w+)[*=]+/g, '$1');
+          }
+          
+        if (this.settings.removeTags) {
+            text = text.replace(/#\w+/g, '');
+          }
+        const html = converter.makeHtml(text).toString();
+        const withDivWrapper = `<!-- directives:[] -->
+            <div id="content">${html}</div>`;
 
-	markdownToHTML(editor: Editor) {
-		const converter = new showdown.Converter();
-		converter.setFlavor('github');
-		converter.setOption('ellipsis', false);
-		const text = editor.getSelection();
-		const noBrackets = text.replace(/\[\[(?:[^\]]+\|)?([^\]]+)\]\]/g, '$1');
-		const html = converter.makeHtml(noBrackets).toString();
-		const withDivWrapper = `<!-- directives:[] -->
-			<div id="content">${html}</div>`;
 		//@ts-ignore
 		const blob = new Blob([withDivWrapper], {
 			//@ts-ignore
@@ -37,9 +60,64 @@ export default class MarkdownToHTML extends Plugin {
 		})];
 		//@ts-ignore
 		navigator.clipboard.write(data);
-	}
+			
+			  
+    }
 
-	onunload() {
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-	}
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+
+    onunload() {
+        // ...
+    }
 }
+
+class MarkdownToHTMLSettingTab extends PluginSettingTab {
+    plugin: MarkdownToHTML;
+  
+    constructor(app: App, plugin: MarkdownToHTML) {
+      super(app, plugin);
+      this.plugin = plugin;
+    }
+  
+    display(): void {
+      let { containerEl } = this;
+      containerEl.empty();
+  
+      new Setting(containerEl)
+        .setName("Remove Wikilink brackets")
+        .setDesc("If enabled, removes wikilink brackets from copied text.")
+        .addToggle(toggle => toggle
+          .setValue(this.plugin.settings.removeBrackets)
+          .onChange(async (value) => {
+            this.plugin.settings.removeBrackets = value;
+            await this.plugin.saveSettings();
+          }));
+  
+      new Setting(containerEl)
+        .setName("Remove text emphasis")
+        .setDesc("If enabled, removes text styling such as bold, italics, and highlights.")
+        .addToggle(toggle => toggle
+          .setValue(this.plugin.settings.removeEmphasis)
+          .onChange(async (value) => {
+            this.plugin.settings.removeEmphasis = value;
+            await this.plugin.saveSettings();
+          }));
+  
+      new Setting(containerEl)
+        .setName("Remove hashtags")
+        .setDesc("If enabled, removes text immediately after a hashtag.")
+        .addToggle(toggle => toggle
+          .setValue(this.plugin.settings.removeTags)
+          .onChange(async (value) => {
+            this.plugin.settings.removeTags = value;
+            await this.plugin.saveSettings();
+          }));
+    }
+  }
+  
